f72547a8b39c2b7782c0f667c6dcbae07069e4bd
harinder2612 <harinder2612@gmail.com>
2020-10-17 23:09:28 +1100

    first commit

---
+++ send.js
@@ -0,0 +1,9 @@
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("path/to/serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://node-firebase-sample-f9c1d.firebaseio.com"
+});
